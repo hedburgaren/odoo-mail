b98ee6144d10856499dc65f4fdfb714534ded87b
Chrille Hedberg <hedberg.chrille@gmail.com>
2026-08-29 13:29:22 +0200

    Fixa VAPID-inställningar: anvand config_parameter for publik nyckel, visa i Settings

--- unified_workspace_pwa_push/models/res_config_settings.py
+++ unified_workspace_pwa_push/models/res_config_settings.py
@@ -17,18 +17,10 @@ class ResConfigSettings(models.TransientModel):
     pwa_push_vapid_public_key = fields.Char(
         string="VAPID publik nyckel",
         readonly=True,
-        compute="_compute_vapid_public_key",
+        config_parameter="unified_workspace_pwa_push.vapid_public_key",
     )
     pwa_push_vapid_subscriber = fields.Char(
         string="VAPID ägare (e-post)",
         config_parameter="unified_workspace_pwa_push.vapid_subscriber_email",
         help="Mailto-adress som äger VAPID-nycklarna.",
     )
-
-    @api.depends_context("company")
-    def _compute_vapid_public_key(self):
-        for setting in self:
-            public_key = self.env["ir.config_parameter"].sudo().get_param(
-                "unified_workspace_pwa_push.vapid_public_key", ""
-            )
-            setting.pwa_push_vapid_public_key = public_key
